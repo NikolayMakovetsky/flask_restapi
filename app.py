9e64505afdf990ccf6073ea8781c0ff0c4a95157
Nikolay <nicholasmakovetsky@gmail.com>
2024-02-13 21:05:25 +0300

    21/05

--- app.py
+++ app.py
@@ -46,8 +46,33 @@ quotes = [
 
 
 @app.route("/quotes")
-def get_qoute():
+def get_qoutes():
    return quotes
+
+
+
+# @app.route('/quotes/<int:id>') # тестовый вариант 1
+# def get_qoute(id):
+#     # return bool(quotes[id])
+#     return quotes[id] if len(quotes) > id else "ошибка 404"
+
+# 3, 5, 6, 8 - cуществующие цитаты
+@app.route('/quotes/<int:quote_id>') # пытаемся привести строку к int
+def get_qoute_by_id(quote_id):
+    """ function return the quote by id.
+        Type of quote is dict -> json"""
+    for quote in quotes:
+        if quote["id"] == quote_id:
+            return quote, 200
+    return f"Quote with id={quote_id} not found", 404
+    
+
+@app.route('/quotes/count')
+def get_count():
+    """return count of quotes"""
+    num = len(quotes)
+    return {"count": num}
+        
 
 
 if __name__ == "__main__":
